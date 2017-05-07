0fa1a49d91e97cac4e060c1a483f49b8a6d134b6
Sukru Tureli <sukrutureli@gmail.com>
2017-05-07 23:59:55 +0300

    First commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+﻿
+/*
+ * GET home page.
+ */
+
+exports.index = function (req, res) {
+    res.render('index', { title: 'Express', year: new Date().getFullYear() });
+};
+
+exports.gorusoneri = function (req, res) {
+    res.render('gorusoneri', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page' });
+};
+
+exports.iletisim = function (req, res) {
+    res.render('iletisim', { title: 'Contact', year: new Date().getFullYear(), message: 'Your contact page' });
+};
